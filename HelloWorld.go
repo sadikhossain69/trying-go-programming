5b30d198a876eea2d5db05519868fab053eeb1ee
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-18 05:27:34 +0600

    simple variable explore

--- HelloWorld.go
+++ HelloWorld.go
@@ -3,5 +3,22 @@ import ("fmt")
 
 
 func main()  {
-	fmt.Println("Hello World")
+
+	var a int  // first method
+
+	a = 55	   // initialization
+
+	var b int = 500  // declaration with initialization
+
+	var c = 100		// declaration with initialization without data type
+
+	var d = "Hello"
+
+	e := "3999"
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
 }
