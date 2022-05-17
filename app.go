3080bbe2d7f3858911473249c8ffc7c158e44efb
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-18 05:35:51 +0600

    local variable, global variable and package variable explore

---
+++ app.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// global vaiable -- (Pascal Case)
+var Val2 int = 100
+
+// Package variable -- (camel case)
+var myValue int = 200
+
+func main()  {
+	
+	// local variable
+	var val1 = 55
+
+	fmt.Println(val1)
+
+}
